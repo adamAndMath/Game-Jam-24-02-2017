99bd2db995b93090a9463fe6621bc66fa832caa4
Adam <adam.and.math@gmail.com>
2017-02-24 20:43:12 +0100

    Added hp and damage

--- Assets/Scripts/Parts/Weapons/PartLaser.cs
+++ Assets/Scripts/Parts/Weapons/PartLaser.cs
@@ -2,11 +2,12 @@
 
 public class PartLaser : PartWeapon
 {
+    public float damage = 0.1F;
     public float width = 0.5F;
     public float maxLength = 20;
     public GameObject laser;
 
-    public override void Update(Player player)
+    public override void UpdateWeapon(Player player)
     {
         bool button = player.InputWeapon;
 
@@ -15,6 +16,12 @@ public class PartLaser : PartWeapon
             RaycastHit2D hit = Physics2D.CircleCast(laser.transform.position, width, transform.up, maxLength);
 
             laser.transform.localScale = new Vector3(1, hit ? hit.distance + width : maxLength, 1);
+
+            if (hit)
+            {
+                Player other = hit.collider.GetComponent<Player>();
+                if (other) other.Damage(damage);
+            }
         }
 
         laser.SetActive(button);
