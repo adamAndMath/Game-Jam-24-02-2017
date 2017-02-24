528906f686c48d23299c49fc414b9d6e502c12f1
Adam <adam.and.math@gmail.com>
2017-02-24 18:59:52 +0100

    Movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movement : MonoBehaviour
+{
+    public float acceleration;
+    public string horizontal;
+    public string vertical;
+
+    private Rigidbody2D rigid;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rigid.AddForce(acceleration*new Vector2(Input.GetAxis(horizontal), Input.GetAxis(vertical)), ForceMode2D.Force);
+    }
+}
