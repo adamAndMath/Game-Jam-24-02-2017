a58841fa1240439b771b338415db709b4b48ed31
Adam <adam.and.math@gmail.com>
2017-02-24 23:53:13 +0100

    Hp Bar

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public Player player;
+    public RectTransform back;
+    public RectTransform front;
+    public float scale;
+
+    void Update()
+    {
+        back.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, scale * player.maxHp);
+        front.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, scale * player.hp);
+    }
+}
