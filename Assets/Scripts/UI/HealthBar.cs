236bcb9d2091c10af7e1d2c826e8d849106d5c9e
Adam <adam.and.math@gmail.com>
2017-02-25 07:38:59 +0100

    Hp bar follows player

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -5,10 +5,12 @@ public class HealthBar : MonoBehaviour
     public Player player;
     public RectTransform back;
     public RectTransform front;
+    public Vector2 offset;
     public float scale;
 
     void Update()
     {
+        back.anchoredPosition = offset + (Vector2)Camera.main.WorldToScreenPoint(player.transform.position);
         back.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, scale * player.maxHp);
         front.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, scale * player.hp);
     }
