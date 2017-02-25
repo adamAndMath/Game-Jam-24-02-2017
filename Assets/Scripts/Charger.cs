cf68f4ed35f4922b6bd4dc6a7a2b3ffb64438a97
Adam <adam.and.math@gmail.com>
2017-02-25 08:47:25 +0100

    Added chager

---
+++ Assets/Scripts/Charger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Charger : MonoBehaviour
+{
+    public BarType barType;
+    public float speed;
+    public Rect rect;
+
+    public enum BarType { Hp, Energy }
+
+    void Update()
+    {
+        Vector2 pos = transform.position;
+        foreach (Collider2D col in Physics2D.OverlapAreaAll(pos + rect.position, pos + rect.position + rect.size))
+        {
+            Player player = col.GetComponent<Player>();
+            if (player)
+            {
+                switch (barType)
+                {
+                    case BarType.Hp: player.hp = Mathf.Min(player.maxHp, player.hp + speed*Time.deltaTime);
+                        break;
+                    case BarType.Energy: player.energy = Mathf.Min(player.maxEnergy, player.energy*Time.deltaTime);
+                        break;
+                }
+
+                throw new Exception();
+            }
+        }
+    }
+}
