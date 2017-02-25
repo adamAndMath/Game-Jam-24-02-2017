e64eef2b230acac2d4839378496e294e2394a7aa
Adam <adam.and.math@gmail.com>
2017-02-25 07:42:12 +0100

    Removed unused code

--- Assets/Scripts/Parts/Move/PartWheel.cs
+++ Assets/Scripts/Parts/Move/PartWheel.cs
@@ -21,10 +21,7 @@ public class PartWheel : PartMove
         player.rigid.drag = drag;
 
         anim.SetBool("IsMoving", movement.sqrMagnitude > 0.01F);
-
-       ChildAnimator.SetBool("OtherWheelIsMoving", movement.sqrMagnitude > 0.01F);
         
-            
         if (movement.sqrMagnitude < 0.01F) return;
 
         float delta = UpdateRotation(player, movement);
